0ba087583f284ad4d7c6f9109de5bd8a1ef6e9f1
denys2 <denys.lempl4@gmail.com>
2016-03-31 21:20:13 +0200

    pisanje datotek

--- app/src/main/java/com/school/denis_niko/projektjanez/QuizActivity.java
+++ app/src/main/java/com/school/denis_niko/projektjanez/QuizActivity.java
@@ -1,7 +1,16 @@
 package com.school.denis_niko.projektjanez;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 /**
  * Created by Denis
@@ -13,6 +22,52 @@ public class QuizActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        final EditText TextQuestion =(EditText) findViewById(R.id.question);
+        //String Question = TextQuestion.getText().toString();
+
+        final EditText TextAnswer =(EditText) findViewById(R.id.answer);
+        //String Answer = TextAnswer.getText().toString();
+
+        Button nexButton = (Button)findViewById(R.id.NextQuestion);
+        // Event listener
+        nexButton.setOnClickListener(
+            new Button.OnClickListener(){
+                public void onClick(View v){
+                    // When the button is clicked
+                    String Question = TextQuestion.getText().toString();
+                    String Answer = TextAnswer.getText().toString();
+                    TextQuestion.setText("");
+                    TextAnswer.setText("");
+                }
+            }
+        );
+
+    }
+    private void WriteFile(String Question,String Answer){
+        try {
+            OutputStreamWriter Writer = new OutputStreamWriter(
+                    this.openFileOutput("QuestionsAnswer.txt", Context.MODE_APPEND));
+            Writer.write(Question + "&" + Answer + "/n");
+            Writer.close();
+        }
+        catch (IOException e){
+            Log.e("Exception", "File write failed: " +e.toString());
+        }
+    }
+
+    /** še testiram **/
+    public void startString(View view)
+    {
+        final EditText TextQuestion =(EditText) findViewById(R.id.question);
+        //String Question = TextQuestion.getText().toString();
+
+        final EditText TextAnswer =(EditText) findViewById(R.id.answer);
+        //String Answer = TextAnswer.getText().toString();
+        String Question = TextQuestion.getText().toString();
+        String Answer = TextAnswer.getText().toString();
+        TextQuestion.setText("");
+        TextAnswer.setText("");
     }
 
 }
