4c7d5976239dc7806fce774d8a4aab8ab7b0ca3e
denys2 <denys.lempl4@gmail.com>
2016-04-10 19:10:05 +0200

    Writing and reading files in quiz

--- app/src/main/java/com/school/denis_niko/projektjanez/QuizActivity.java
+++ app/src/main/java/com/school/denis_niko/projektjanez/QuizActivity.java
@@ -3,13 +3,20 @@ package com.school.denis_niko.projektjanez;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 /**
@@ -17,33 +24,75 @@ import java.io.OutputStreamWriter;
  */
 
 public class QuizActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
-        final EditText TextQuestion =(EditText) findViewById(R.id.question);
+        final EditText InputQuestion =(EditText) findViewById(R.id.question);
         //String Question = TextQuestion.getText().toString();
-
-        final EditText TextAnswer =(EditText) findViewById(R.id.answer);
+        final EditText InputAnswer =(EditText) findViewById(R.id.answer);
         //String Answer = TextAnswer.getText().toString();
+        final Button nexButton = (Button)findViewById(R.id.NextQuestion);
 
-        Button nexButton = (Button)findViewById(R.id.NextQuestion);
+
+        final TextView TextQuestion = (TextView) findViewById(R.id.QuestionText);
+        final TextView TextAnswer = (TextView) findViewById(R.id.AnwserText);
+        final Button AnswerButton = (Button)findViewById(R.id.AnwserAndNext);
+
         // Event listener
         nexButton.setOnClickListener(
             new Button.OnClickListener(){
                 public void onClick(View v){
                     // When the button is clicked
-                    String Question = TextQuestion.getText().toString();
-                    String Answer = TextAnswer.getText().toString();
-                    TextQuestion.setText("");
-                    TextAnswer.setText("");
+                    String Question = InputQuestion.getText().toString();
+                    String Answer = InputAnswer.getText().toString();
+                    WriteFile(Question,Answer);
+                    InputQuestion.setText("");
+                    InputAnswer.setText("");
                 }
             }
         );
 
+        final Button StartQuizButton =(Button)findViewById(R.id.StartQuiz);
+        StartQuizButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v)
+                    {
+                        InputAnswer.setVisibility(View.GONE);
+                        InputQuestion.setVisibility(View.GONE);
+                        nexButton.setVisibility(View.GONE);
+                       // StartQuizButton.setVisibility(View.GONE);
+
+                        TextQuestion.setVisibility(View.VISIBLE);
+                        AnswerButton.setVisibility(View.VISIBLE);
+                    }
+
+                }
+        );
+
+        AnswerButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v)
+                    {
+                        TextAnswer.setVisibility(View.VISIBLE);
+                        TextAnswer.setText(readFile("QuestionsAnswer.txt"));
+                    }
+                }
+
+        );
+
     }
+    //end of on create
+/*
+    private void saveData(TextView InputQuestion)
+    {
+
+    }
+    private void textboxClear()
+    {
+
+    }
+*/
     private void WriteFile(String Question,String Answer){
         try {
             OutputStreamWriter Writer = new OutputStreamWriter(
@@ -56,18 +105,31 @@ public class QuizActivity extends Activity {
         }
     }
 
-    /** še testiram **/
-    public void startString(View view)
-    {
-        final EditText TextQuestion =(EditText) findViewById(R.id.question);
-        //String Question = TextQuestion.getText().toString();
+   private String readFile(String fileName){
+       String readString="";
+       try{
+           InputStream inputStream = openFileInput(fileName);
+           if(inputStream != null){
+               InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+               BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+               StringBuilder stringBuilder = new StringBuilder();
 
-        final EditText TextAnswer =(EditText) findViewById(R.id.answer);
-        //String Answer = TextAnswer.getText().toString();
-        String Question = TextQuestion.getText().toString();
-        String Answer = TextAnswer.getText().toString();
-        TextQuestion.setText("");
-        TextAnswer.setText("");
+               while((readString = bufferedReader.readLine())!= null){
+                   stringBuilder.append(readString);
+               }
+               inputStream.close();
+               readString = stringBuilder.toString();
+           }
+       }catch (FileNotFoundException e){
+           Log.e("login activity", "File not found: " + e.toString());
+       } catch (IOException e){
+           Log.e("login activity", "Can not read file: " + e.toString());
+       }
+       return readString;
+   }
+    public boolean deleteFile(String fileName) {
+        File dir = getFilesDir();
+        File file = new File(dir, fileName);
+        return file.delete();
     }
-
 }
